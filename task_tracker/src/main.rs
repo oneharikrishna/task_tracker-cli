0fa1fad42156091853ccda518f4fd133398ec8cf
Harikrishna T <harikrishna09876543@gmail.com>
2025-08-11 00:01:14 +0530

    main.rs

--- task_tracker/src/main.rs
+++ task_tracker/src/main.rs
@@ -1,3 +1,21 @@
+enum Status {
+    Todo,
+    InProgress,
+    Done,
+}
+
+enum Command {
+    Add { task: String },
+    Update { id: u32 },
+    Delete { id: u32 },
+    MarkInProgress { id: u32 },
+    MarkDone { id: u32 },
+    List { status: Option<Status> },
+}
+
+struct Cli {
+    command : Command,
+}
 fn main() {
     println!("Hello, world!");
 }
